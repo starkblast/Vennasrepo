7244e06cabac344e64eaca2ceb7f29fabfc68821
Cablefox <kristina1koch@yahoo.com>
2016-12-13 20:14:57 +0200

    Nupust nikastamine

---
+++ SKRIPTID!/NUPUSTNIKASTANUD.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NUPUSTNIKASTANUD : MonoBehaviour {
+    public bool kollanenupp = false;
+    public bool lillanupp = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnMouseDown()
+    {
+        if (this.name == "KOLLANENUPP")
+        {
+            GameObject.Find("see kes kaitseb").GetComponent<signlanguage>().kollanenupp = true;
+        }
+        if (this.name == "LILLANUPP")
+        {
+            GameObject.Find("see kes kaitseb").GetComponent<signlanguage>().lillanupp = true;
+        }
+    }
+    void OnMouseUp()
+    {
+        kollanenupp = false;
+        lillanupp = false;
+    }
+}
