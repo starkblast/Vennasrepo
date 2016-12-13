176b57b9b4167671f719462367e9bab1b94db48f
starkblast <chazzykaku@gmail.com>
2016-12-13 15:20:48 +0200

    erinevab objektid erineval otstarbel

---
+++ JooksmisScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JooksmisScript : MonoBehaviour {
+	public float speed = 0.02f;
+	void Start () {
+		
+	}
+	
+
+	void Update () {
+		transform.position = new Vector3 (transform.position.x + speed, transform.position.y, transform.position.z);
+	}
+}
---
+++ KaitsmisScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KaitsmisScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter2D (Collision2D c) {
+		Debug.Log (c.transform.name);
+		--GameObject.Find ("objekt kus on erinev info").GetComponent<ElamisScript> ().elud;
+	}
+}
